8df309d789298e0f2e322fe2d113301469dadda7
refdd <titorefat76@gmail.com>
2023-05-21 19:38:46 +0300

    push tot

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 // vars
 const headerSection = document.querySelector(".headerSection");
 const contantHeader = document.querySelector(".contantHeader");
-
+const closeButton = document.querySelector(".closeOffer");
+const boxOffer = document.querySelector(".buttonOfferSection");
+const buttonView = document.querySelector(".buttonView");
+// close offer box
+closeButton &&
+  closeButton.addEventListener("click", () => {
+    boxOffer.classList.add("hidden");
+    buttonView.classList.add("move");
+  });
 document.addEventListener("DOMContentLoaded", function () {
   setTimeout(function () {
     headerSection && headerSection.classList.add("active");
